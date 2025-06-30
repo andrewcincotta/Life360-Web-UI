3fecdb035605853f9c55b6574c38cda07216f68a
Andrew Cincotta <cincotta.a@northeastern.edu>
2025-06-29 21:04:40 -0400

    update env

--- run.py
+++ run.py
@@ -1,5 +1,17 @@
+import os
+from dotenv import load_dotenv
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
-    
+    load_dotenv()
+
+    # Grab with defaults and proper types
+    host = os.getenv("FASTAPI_HOST", "127.0.0.1")
+    port = int(os.getenv("FASTAPI_PORT", 8000))
+
+    uvicorn.run(
+        "app.main:app",
+        host=host,
+        port=port,
+        reload=True,
+    )
